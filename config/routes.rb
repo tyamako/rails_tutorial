32629a85ed36c9ffea9facf532291967076c7f24
koichiro tamaki <koichiro tamaki>
2016-12-12 21:54:43 +0900

    tutorial7_1_3

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ SampleApp::Application.routes.draw do
   # get "static_pages/help"
   # get "static_pages/about"
   # get "static_pages/contact"
+  resources :users
   root 'static_pages#home'
   match '/signup', to: 'users#new', via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
